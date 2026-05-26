b85dff61a9e7f2670b60ae401696b531556cac50
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2026-05-26 16:32:23 -0300

    feat(module-1/session-1): decimaal and double usage rules

--- Program.cs
+++ Program.cs
@@ -8,15 +8,24 @@
 // Console.WriteLine("Region (cpalesced):{displayRegion}");
 // region ??= "Addis Abeba";
 // Console.WriteLine("Reion(assigned):{region}");
-string studentName = "Abebe";
-string studentId = "STU-001";
-int enrollmentCount = 3;
-decimal grantAmount = 1999.99m;
-DateTime enrolledAt = DateTime.UtcNow;
-string? campusRegion = null;
+// string studentName = "Abebe";
+// string studentId = "STU-001";
+// int enrollmentCount = 3;
+// decimal grantAmount = 1999.99m;
+// DateTime enrolledAt = DateTime.UtcNow;
+// string? campusRegion = null;
 
-Console.WriteLine($"Student: {studentName} {studentId}");
-Console.WriteLine($"Enrollment Count: {enrollmentCount}");
-Console.WriteLine($"Grant Amount: {grantAmount:C}");
-Console.WriteLine($"Enrolled At: {enrolledAt: yyyy-MM-dd HH:mm:ss} UTC");
-Console.WriteLine($"Campus Region: {campusRegion ?? "Not specified"}");
+// Console.WriteLine($"Student: {studentName} {studentId}");
+// Console.WriteLine($"Enrollment Count: {enrollmentCount}");
+// Console.WriteLine($"Grant Amount: {grantAmount:C}");
+// Console.WriteLine($"Enrolled At: {enrolledAt: yyyy-MM-dd HH:mm:ss} UTC");
+// Console.WriteLine($"Campus Region: {campusRegion ?? "Not specified"}");
+
+// double grantPerStudent = 1999.99;
+// double totalAllocation = grantPerStudent * 100_000;
+// Console.WriteLine($"Total allocated (double): {totalAllocation:R}");
+
+
+decimal grantPerStudent = 1999.99m;
+decimal totalAllocation = grantPerStudent * 100_000;
+Console.WriteLine($"Total allocated (double): {totalAllocation}");
--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebee3a8501d080eae939994d260492e4d76c08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82182e1209024671871147d61bc66a684804c67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
