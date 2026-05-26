4ebee3a8501d080eae939994d260492e4d76c08b
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2026-05-26 04:39:48 -0300

    feat: initial project scaffold (C# .NET 10.0 console app)

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿echo "# TmsCore" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Hawas-Muhaba/TmsCore.git
+git push -u origin main
