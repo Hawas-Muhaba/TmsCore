84c88a3c4cb1b7c46bb5cf8a9910720cc9d5dc79
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2026-06-03 01:52:36 -0300

    feat(module-1/session-3): implemented integration exercise

--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cec1a9213cb58c1b05353887a36f2c30445fa5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5cc037ae52ab5bb0fa397fc499d25bb9e67cd7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
